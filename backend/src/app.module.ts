a19b2f2b6f460af82c0cfcf87623cd96824c0cde
AlejoDiazLote <alejandrodiazlote@gmail.com>
2026-04-21 13:23:49 -0500

    version base del back conectado a la base de datos

---
+++ backend/src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { EventsModule } from './modules/events/events.module';
+import { SessionsModule } from './modules/sessions/sessions.module';
+import { RegistrationsModule } from './modules/registrations/registrations.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: Number(configService.get<string>('DATABASE_PORT')),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+
+    AuthModule,
+    UsersModule,
+    RolesModule,
+    EventsModule,
+    SessionsModule,
+    RegistrationsModule,
+  ],
+})
+export class AppModule {}
